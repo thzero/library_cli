9914ef634d48b9c8cd9cd80a13dce84498b78b2c
thzero <thzero@users.noreply.github.com>
2024-02-24 13:21:40 -0600

    updates

--- api.js
+++ api.js
@@ -1,137 +1,46 @@
-#!/usr/bin/env node
-import fs from 'fs';
-import path from 'path';
+import { updateVersion } from '@thzero/library_cli/api.js';
 
-import dayjs from 'dayjs';
-import { nanoid, customAlphabet } from 'nanoid';
-import readline from 'readline';
-import shortUUID from 'short-uuid';
-import { v4 as uuidv4 } from 'uuid';
+import ActionBuildService from './index.js';
 
-const uuidTranslator = shortUUID();
-
-// https://timber.io/blog/creating-a-real-world-cli-app-with-node/
-
-export function generateId(number, type, length, alphabet) {
-	let results = [];
-	for (let i = 0; i < number; i++) {
-		if ('nano' === type)
-			results.push(generateNanoId(length, alphabet));
-		else if ('short' === type)
-			results.push(generateShortId());
-		else
-			results.push(generateLongId());
+class VersionActionBuildService extends ActionBuildService {
+	constructor() {
+		super();
 	}
-	return results;
-}
 
-function generateNanoId(length, alphabet) {
-	let temp = nanoid;
-	if (length && alphabet)
-		temp = customAlphabet(alphabet, length);
-	if (alphabet)
-		temp = customAlphabet(alphabet);
-	
-	if (length)
-		return temp(length);
-	return temp();
-}
+	async _process(correlationId, buildLog, repo) {
+		let pi = buildLog.args.pi === true;
+		let major = null;
+		if (!String.isNullOrEmpty(buildLog.args.major)) {
+			major = buildLog.args.major;
+			pi = false;
+		}
+		let minor = null;
+		if (!String.isNullOrEmpty(buildLog.args.minor)) {
+			minor = buildLog.args.minor;
+			pi = false;
+		}
 
-function generateLongId() {
-	return uuidv4();
-}
-
-function generateShortId() {
-	return uuidTranslator.new();
-}
-
-function question(q, acceptable) {
-	const rl = readline.createInterface( {
-		input: process.stdin,
-		output: process.stdout
-	} );
-
-	rl.setPrompt(q);
-	rl.prompt();
-
-	return new Promise(( resolve , reject) => {
-		let response;
-		rl.on('line', (userInput) => {
-			response = userInput;
-			rl.close();
+		const results = await updateVersion({
+			packagePath: repo.pathPackage,
+			major: major,
+			minor: minor,
+			pi: pi
 		});
-		rl.on('close', () => {
-			resolve(response === acceptable);
-		});
-	});
-}
+		this._logger.info2('\t\t' + (results.message || results.error) ? results.message ? results.message : '' : results.error ? results.error : 'failed');
 
-export async function updateVersion(args) {
-	args = args ? args : {};
+		if (results.success)
+			repo.dirty = true;
 
-	let packagePath = !_isEmpty(args.packagePath) ? args.packagePath : process.cwd();
-	if (!packagePath.includes('package.json'))
-		packagePath = path.join(packagePath, 'package.json');
-	// const packageJson = require(packagePath);
-	let packageJson = await fs.promises.readFile(packagePath);
-	packageJson = JSON.parse(packageJson);
-	// console.log(packageJson);
-
-	if (!args.major)
-		args.major = Number(packageJson.version_major || 0);
-	// console.log(args);
-	if (!args.minor)
-		args.minor = Number(packageJson.version_minor || 0);
-	// console.log(args);
-	if (!args.patch)
-		args.patch = Number(packageJson.version_patch || 0);
-	// console.log(args);
-	if (!args.date)
-		args.date = dayjs().format('MM/DD/YYYY');
-	// console.log(args);
-	if (args.silent === undefined)
-		args.silent = true;
-	// console.log(args);
-
-	if (args.pi)
-		args.patch = args.patch + 1;
-	// console.log(args);
-
-	const version = `${args.major}.${args.minor}.${args.patch}`;
-
-	if (!args.silent) {
-		const value = await question(`Updating with version '${version}' and date '${args.date}'.\nDo you want to proceed? [y/n] `, 'y');
-		if (!value)
-			return { success: true, message: 'No updates applied.' };
+		return this._successResponse(results, correlationId);
 	}
 
-	packageJson.version = version;
-	packageJson.version_major = args.major;
-	packageJson.version_minor = args.minor;
-	packageJson.version_patch = args.patch;
-	packageJson.version_date = args.date;
-	// console.log(packageJson);
+	get _prefix() {
+		return '';
+	}
 
-	const output = JSON.stringify(packageJson, null, 2);
-	// console.log(output);
-	try {
-		fs.promises.writeFile(packagePath, output);
+	get _step() {
+		return 'version';
 	}
-	catch (err) {
-		return { success: false, error: err };
-	}
-	return { success: true, message: `Updated version with '${args.major}.${args.minor}.${args.patch}', '${args.date}'.` };
 }
 
-function _isEmpty(value) {
-	if (value === null || value === undefined)
-		return true;
-
-	if (!(typeof value === 'string'))
-		return true;
-
-	if (value.length === 0)
-		return true;
-	
-	return false;
-}
+export default VersionActionBuildService;
